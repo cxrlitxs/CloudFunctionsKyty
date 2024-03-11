328c083548a9eacffc22346f1d04c8fdd5e713a9
cxrlitxs <carloscastanedo.88@gmail.com>
2024-03-11 02:38:01 +0100

    Añadida función Cloud 'addpost' para inserción de posts en Firestore.
    Implementa la función Cloud 'addpost' para manejar solicitudes HTTP POST.
    Esta función recibe parámetros 'nickName', 'body' y 'title' de la solicitud,
    inserta un nuevo post en la colección 'pruebaPosts' de Firestore,
    y responde con el ID del post insertado. Incluye manejo de errores para capturar
    y responder adecuadamente a las excepciones durante la inserción del post.

--- functions/index.js
+++ functions/index.js
@@ -9,11 +9,32 @@
 
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const {initializeApp} = require("firebase-admin/app");
+const {getFirestore, FieldValue} = require("firebase-admin/firestore");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+//Inicialización de la app con las creedenciales
+initializeApp();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Exporta la función 'addpost' como una Cloud Function para Firebase
+exports.addpost = onRequest(async (req, res) => {
+    // Obtiene los parámetros 'nickName', 'body' y 'title' de la solicitud HTTP
+    const nickName = req.query.nickName;
+    const body = req.query.body;
+    const title = req.query.title;
+  
+    try {
+        // Inserta un nuevo documento en la colección 'pruebaPosts' en Firestore
+        // Utiliza el SDK de Admin de Firebase para acceder a Firestore
+        const writeResult = await getFirestore()
+            .collection("pruebaPosts")
+            .add({nickName: nickName, body: body, title: title});
+
+        // Envía una respuesta JSON indicando que el post fue insertado correctamente
+        res.json({result: `Post con ID: ${writeResult.id} fue insertado correctamente.`});
+    } catch (error) {
+        // Maneja cualquier error que ocurra durante la inserción de datos o la recuperación
+        console.error("Error al insertar el mensaje: ", error);
+        // Envía una respuesta de estado 500 (Error Interno del Servidor) si ocurre un error
+        res.status(500).send('Error al insertar el post')
+    }
+});
